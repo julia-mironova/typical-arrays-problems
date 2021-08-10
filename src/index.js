f255f6cf6c655a19c4f39b0718ce7f84109d87a6
Yuliya Mironova <rabkevich15@gmail.com>
2021-08-11 00:49:29 +0200

    feat:typical array problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
-exports.min = function min (array) {
-  return 0;
+function minimaze(array) {
+    if (typeof array !== "object" || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+minimaze();
+exports.min = minimaze;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function maximaze(array) {
+    if (typeof array !== "object" || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
+};
+
+exports.avg = function avgergaze(array) {
+    if (typeof array !== "object" || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let avg = sum / array.length;
+        return avg;
+    }
+};
---
+++ src/index2/index2.js
@@ -0,0 +1,3 @@
+const jopa = require("../index.js");
+/*console.log(11);*/
+console.log(jopa.min([1, 2, 3, 4]));
